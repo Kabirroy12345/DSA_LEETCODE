a9fbf50557a4774ab6a2c5d69c6a19ef3e6ff17e
Kabir Roy <144699626+Kabirroy12345@users.noreply.github.com>
2026-07-11 00:28:05 +0530

    Time: 3 ms (43.59%) | Memory: 8.1 MB (94.07%) - LeetSync

---
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string ans="";
+        for(int i=0;i<command.length();i++){
+            if(command[i]=='G'){
+                ans+='G';
+            }
+            if(command[i]=='('&&command[i+1]==')'){
+                ans+='o';
+            }
+            if(command[i]=='('&&command[i+1]=='a'&&command[i+2]=='l'&&command[i+3]==')'){
+                ans+='a';
+                ans+='l';
+            }
+        }
+        return ans;
+    }
+};
